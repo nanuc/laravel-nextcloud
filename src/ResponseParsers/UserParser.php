0be3f14d98d604569a00f8203f3368a3c58ae2c0
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2023-06-20 11:10:38 +0200

    Update UserParser.php

--- src/ResponseParsers/UserParser.php
+++ src/ResponseParsers/UserParser.php
@@ -2,7 +2,6 @@
 
 namespace Nanuc\Nextcloud\ResponseParsers;
 
-use Illuminate\Support\Arr;
 use Nanuc\Nextcloud\Results\User;
 
 class UserParser extends ResponseParser
@@ -10,34 +9,17 @@ class UserParser extends ResponseParser
     public function parse()
     {
         $userResult = new User();
-        
-        $xpath = $this->xpath;
-        $data = [];
 
-        $dataNodes = $this->xpath->query("/ocs/data/*");
-        foreach($dataNodes as $node) {
-            $nodeName = $node->nodeName;
-            $nodeValue = $node->nodeValue;
-
-            // If the node has children, loop over its children and add them to a subarray
-            if($node->hasChildNodes() && $node->childNodes->length > 1) {
-                $subarray = [];
-                foreach($node->childNodes as $childNode) {
-                    if ($childNode->nodeType === XML_ELEMENT_NODE) {
-                        $subarray[$childNode->nodeName] = $childNode->nodeValue;
-                    }
-                }
-                $data[$nodeName] = $subarray;
-            }
-            else {
-                $data[$nodeName] = $nodeValue;
-            }
+        $elements = $this->xpath->query('/ocs/data/groups/element');
+        $groups = [];
+        foreach ($elements as $element) {
+            $groups[] = $element->nodeValue;
         }
 
-        $userResult->id = Arr::get($data, 'id');
-        $userResult->email = Arr::get($data, 'email');
-        $userResult->enabled = Arr::get($data, 'enabled') == 1;
-        $userResult->groups = is_array(Arr::get($data, 'groups')) ? array_values(Arr::get($data, 'groups')) : [];
+        $userResult->id = $this->xpath->evaluate('string(/ocs/data/id)');
+        $userResult->email = $this->xpath->evaluate('string(/ocs/data/email)');
+        $userResult->enabled = $this->xpath->evaluate('string(/ocs/data/enabled)') === '1';
+        $userResult->groups = $groups;
 
         return $this->finalize($userResult);
     }
